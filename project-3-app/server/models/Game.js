c2fa0ba343c15ad0ebf6fc066fb0439606c04a5f
Part-time-Dan <126934952+Part-time-Dan@users.noreply.github.com>
2023-09-22 20:57:22 -0600

    adjust Game fetch API and model

--- project-3-app/server/models/Game.js
+++ project-3-app/server/models/Game.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
 const gameSchema = new mongoose.Schema({
+  // storing an _id because the API will likely have id's the model will need to reference to match user posts to games
+  // _id: ObjectId
+  
   // fields will be fetched and defined from the API
   title: {
     type: String,
