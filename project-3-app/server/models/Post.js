9d36639478dd10813e87ee1ab9dccec1260234b7
Part-time-Dan <126934952+Part-time-Dan@users.noreply.github.com>
2023-09-23 15:00:58 -0600

    refactor/debug models, build typeDefs

--- project-3-app/server/models/Post.js
+++ project-3-app/server/models/Post.js
@@ -1,32 +1,42 @@
 const { Schema, model } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
-const postSchema = new Schema({
-    postText: {
-        type: String,
-        required: 'You need to leave a thought!',
-        minlength: 1,
-        maxlength: 500,
-        trim: true,
-    },
-    postAuthor: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-        get: (timestamp) => dateFormat(timestamp),
-    },
-    comments: [
-        {
+const postSchema = new Schema(
+    {
+        postTitle: {
+            type: String,
+            required: 'Posts require a title!',
+            minlength: 1,
+            maxlength: 80,
+            trim: true,
+        },
+        postText: {
+            type: String,
+            required: 'Posts require content!',
+            minlength: 1,
+            maxlength: 500,
+            trim: true,
+        },
+        postAuthor: {
             type: Schema.Types.ObjectId,
-            ref: 'Comment',
-        }
-    ],
+            ref: 'User',
+            required: true,
+            trim: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (timestamp) => dateFormat(timestamp),
+        },
+        comments: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Comment',
+            }
+        ],
 
-});
+    }
+);
 
 const Post = model('Post', postSchema);
 
