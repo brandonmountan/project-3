463281963ba9eea1484c7a091e1d1d5312128179
Bridger Daw <bridgerdaw@yahoo.com>
2023-09-27 19:38:47 -0600

    working on adding donate button

--- project-3-app/client/src/components/Header.js
+++ project-3-app/client/src/components/Header.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import Badge from 'react-bootstrap/Badge';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import Badge from "react-bootstrap/Badge";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { loadStripe } from "@stripe/stripe-js";
 
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_KEY);
+
+async function donation() {
+  const stripe = await stripePromise;
+}
 
 function Header() {
   return (
-    <div class='p-4'>
+    <div class="p-4">
       <h1 class="text-center">
         PostGame <Badge bg="secondary">Welcome!</Badge>
       </h1>
+      <button onClick={donation}>Donate</button>
     </div>
   );
 }
