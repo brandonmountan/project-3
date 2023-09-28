0f0e6f571038d6765b19885784091e3831136f64
Reiley Burnitt <burnittreiley@gmail.com>
2023-09-27 21:06:00 -0600

    pushing to develop

--- project-3-app/client/src/components/pages/Profile.js
+++ project-3-app/client/src/components/pages/Profile.js
@@ -1,4 +1,3 @@
-// <<<<<<< HEAD
 // import React from "react";
 // import { Navigate, useParams } from "react-router-dom";
 // import { useQuery } from "@apollo/client";
@@ -41,19 +40,3 @@
 // // };
 
 // export default Profile;
-// =======
-// import React from 'react';
-// import Card from 'react-bootstrap/Card';
-// import { useQuery } from '@apollo/client';
-
-
-// function Profile() {
-//   return (
-//     <Card class="m-5">
-//       <Card.Body class="p-5">Hello</Card.Body>
-//     </Card>
-//   );
-// }
-
-// export default Profile;
-// >>>>>>> 07d0db4942ee507c7cb6bd193e74517e7d2a5f9d
