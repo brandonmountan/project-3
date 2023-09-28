67cdf0f5d64d4fdc29ed888758f0d9fa9c4e0937
Brandon Mountan <brandonmountan1@gmail.com>
2023-09-27 21:43:42 -0600

    starting over

--- project-3-app/client/src/components/pages/Login.js
+++ project-3-app/client/src/components/pages/Login.js
@@ -1,24 +1,13 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import { LOGIN } from '../utils/mutations';
-import { useMutation } from '@apollo/client';
-
-import Auth from '../utils/auth';
-
 // import { LOGIN } from '../utils/mutations';
 import '../../../src/App.css';
-
 
 const Login = (props) => {
 
   return (
-
-    <div className="m-5">
-      {data ? (
-        <p>success</p>
-      ) : (
-        <Form className="p-5">
+    
     <div className='forms-container'>
       <div className='login-form'>
         {/* Login Form */}
@@ -34,13 +23,6 @@ const Login = (props) => {
         <Button variant="primary" type="submit">
           Login
         </Button>
-      </Form>
-      )}
-      {error && (
-              <div className="my-3 p-3 bg-danger text-white">
-                {error.message}
-              </div>
-            )}
       </div>
     </div>
     
