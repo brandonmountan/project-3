278765a36445a78e6b4d1d0c108304a7e9519944
Part-time-Dan <126934952+Part-time-Dan@users.noreply.github.com>
2023-09-28 14:04:39 -0600

    addComment resolver working

--- project-3-app/server/schemas/resolvers.js
+++ project-3-app/server/schemas/resolvers.js
@@ -3,12 +3,13 @@ const { User, Post, Comment } = require("../models");
 const { signToken } = require("../utils/auth");
 const Stripe = require("stripe");
 const stripe = Stripe(
-  "sk_test_51NruVnLbZqu6Vpn3o85H4sr4avu3L7WKZtaBJhVx52qVPs0UG4OqplpdkSCNDyK4w9DpXRPXAXpdXrmkzxFmGJF800GFrXttl6"
+    "sk_test_51NruVnLbZqu6Vpn3o85H4sr4avu3L7WKZtaBJhVx52qVPs0UG4OqplpdkSCNDyK4w9DpXRPXAXpdXrmkzxFmGJF800GFrXttl6"
 );
 console.log(stripe);
 
 const resolvers = {
     Query: {
+        // tested- working "getAllUsers"
         users: async () => {
             return User.find().populate('posts');
         },
@@ -24,7 +25,7 @@ const resolvers = {
         posts: async (parent, { userId }) => {
             const params = userId ? { postAuthor: userId } : {};
             return Post.find(params).sort({ createdAt: -1 })
-            .populate('postAuthor');
+                .populate('postAuthor');
         },
 
         // tested- working "getSinglePost"
@@ -38,6 +39,8 @@ const resolvers = {
                     },
                 }); // get additional user data from author ID
         },
+
+        // tested- working "getMyProfile"
         me: async (parent, args, context) => {
             if (context.user) {
                 return User.findOne({ _id: context.user._id })
@@ -68,33 +71,33 @@ const resolvers = {
                 // look up user by email address. 
                 const user = await User.findOne({ email });
 
-        // if no user with that email address, throw error
-        if (!user) {
-          throw new AuthenticationError(
-            "No user found with this email address"
-          );
-        }
+                // if no user with that email address, throw error
+                if (!user) {
+                    throw new AuthenticationError(
+                        "No user found with this email address"
+                    );
+                }
 
-        // otherwise execute the `isCorrectPassword` instance method and check if the correct password was provided
-        const correctPw = await user.isCorrectPassword(password);
+                // otherwise execute the `isCorrectPassword` instance method and check if the correct password was provided
+                const correctPw = await user.isCorrectPassword(password);
 
-        // password is incorrect, throw error
-        if (!correctPw) {
-          throw new AuthenticationError("Incorrect credentials");
-        }
+                // password is incorrect, throw error
+                if (!correctPw) {
+                    throw new AuthenticationError("Incorrect credentials");
+                }
 
-        // email and password are correct, sign the user into the application with a JWT
-        const token = signToken(user);
+                // email and password are correct, sign the user into the application with a JWT
+                const token = signToken(user);
 
-        // `Auth` object of token
-        return { token, user };
-      } catch (error) {
-        throw new Error(`Error logging in: ${error.message}`);
-      }
-    },
+                // `Auth` object of token
+                return { token, user };
+            } catch (error) {
+                throw new Error(`Error logging in: ${error.message}`);
+            }
+        },
 
         // THE USER YOU CHOOSE TO GENERATE YOUR TOKEN ON THE LOGIN ROUTE WILL BE THE USER AFFECTED BY ALL AUTH ROUTES, SO IT DOESN'T MATTER WHAT "AUTHOR" ID IS USED IN THE APOLLO VARIABLES
-  
+
         // tested- working
         addPost: async (parent, { postTitle, postText, gameId }, context) => {
             console.log("test addPost");
@@ -109,16 +112,16 @@ const resolvers = {
                     game: gameId // or whatever we use to identify the games through the API
                 });
 
-        console.log("author user id is: " + context.user._id);
-        console.log("author username is: " + context.user.username);
+                console.log("author user id is: " + context.user._id);
+                console.log("author username is: " + context.user.username);
 
-        await User.findOneAndUpdate(
-          { _id: context.user._id },
-          { $addToSet: { posts: post._id } }
-        );
+                await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { posts: post._id } }
+                );
 
                 const newPost = await Post.findById(post).populate('postAuthor'); //to display username from postAuthor _id
-                
+
                 console.log("user id is: " + context.user._id)
                 return newPost;
 
@@ -142,33 +145,33 @@ const resolvers = {
                     throw new AuthenticationError('You are not authorized to update this post');
                 }
 
-        const updates = {
-          postTitle,
-          postText,
-        };
+                const updates = {
+                    postTitle,
+                    postText,
+                };
 
-        // if gameId is provided, update the game association
-        if (gameId) {
-          updates.game = gameId;
-        }
+                // if gameId is provided, update the game association
+                if (gameId) {
+                    updates.game = gameId;
+                }
 
-        const updatedPost = await Post.findOneAndUpdate(
-          { _id: postId },
-          { $set: updates },
-          { new: true }
-        );
+                const updatedPost = await Post.findOneAndUpdate(
+                    { _id: postId },
+                    { $set: updates },
+                    { new: true }
+                );
 
-        if (!updatedPost) {
-          throw new Error("Post not found");
-        }
+                if (!updatedPost) {
+                    throw new Error("Post not found");
+                }
 
-        return updatedPost;
-      } catch (error) {
-        throw new Error(`Error updating post: ${error.message}`);
-      }
-    },
+                return updatedPost;
+            } catch (error) {
+                throw new Error(`Error updating post: ${error.message}`);
+            }
+        },
 
-        // tested- working
+        // tested- working "removePost"
         removePost: async (parent, { postId }, context) => {
             console.log("test removePost");
             if (!context.user) {
@@ -180,33 +183,33 @@ const resolvers = {
                     postAuthor: context.user._id
                 });
 
-        if (!existingPost) {
-          throw new AuthenticationError(
-            "You are not authorized to remove this post"
-          );
-        }
-        const deletedPost = await Post.findOneAndDelete({ _id: postId });
+                if (!existingPost) {
+                    throw new AuthenticationError(
+                        "You are not authorized to remove this post"
+                    );
+                }
+                const deletedPost = await Post.findOneAndDelete({ _id: postId });
 
-        if (!deletedPost) {
-          throw new Error("Post not found");
-        }
+                if (!deletedPost) {
+                    throw new Error("Post not found");
+                }
 
-        // remove the reference to this post from the user
-        await User.findOneAndUpdate(
-          { _id: deletedPost.postAuthor },
-          { $pull: { posts: postId } }
-        );
+                // remove the reference to this post from the user
+                await User.findOneAndUpdate(
+                    { _id: deletedPost.postAuthor },
+                    { $pull: { posts: postId } }
+                );
 
-        // remove the reference to this post from the game
-        if (deletedPost.game) {
-          await Game.findOneAndUpdate(
-            { _id: deletedPost.game },
-            { $pull: { posts: postId } }
-          );
-        }
+                // remove the reference to this post from the game
+                if (deletedPost.game) {
+                    await Game.findOneAndUpdate(
+                        { _id: deletedPost.game },
+                        { $pull: { posts: postId } }
+                    );
+                }
 
-        // do we want to delete all comments associated with a Post, or make a placeholder "user removed post" that leaves comments visible under the deleted post?
-        // await Comment.deleteMany({ post: postId });
+                // do we want to delete all comments associated with a Post, or make a placeholder "user removed post" that leaves comments visible under the deleted post?
+                // await Comment.deleteMany({ post: postId });
 
                 return deletedPost;
             } catch (error) {
@@ -214,122 +217,129 @@ const resolvers = {
             }
         },
 
+        // tested- working "addComment"
+        addComment: async (parent, { postId, commentText }, context) => {
+            console.log("test addComment");
 
-    addComment: async (
-      parent,
-      { postId, commentText, commentAuthor },
-      context
-    ) => {
-      console.log("test addComment");
-      if (!context.user) {
-        throw new AuthenticationError(
-          "You need to be logged in to comment on other posts"
-        );
-      }
-      try {
-        const updatedPost = await Post.findOneAndUpdate(
-          { _id: postId },
-          {
-            $addToSet: {
-              comments: {
-                commentText,
-                commentAuthor: context.user._id,
-              },
-            },
-          },
-          {
-            new: true,
-            runValidators: true,
-          }
-        );
+            if (!context.user) {
+                throw new AuthenticationError(
+                    "You need to be logged in to comment on other posts"
+                );
+            }
 
-        if (!updatedPost) {
-          throw new Error("Post not found");
-        }
+            try {
+                const comment = await Comment.create({
+                    commentText,
+                    commentAuthor: context.user._id,
+                    post: postId,
+                });
 
-        return updatedPost;
-      } catch (error) {
-        throw new Error(`Error adding comment: ${error.message}`);
-      }
+                // query created comment to populate author data
+                const newComment = await Comment.findById(comment._id).populate("commentAuthor");
+
+                // add comment to comments array inside Post
+                const updatedPost = await Post.findOneAndUpdate(
+                    { _id: postId },
+                    {
+                        $addToSet: { comments: newComment._id },
+                    },
+                    {
+                        new: true,
+                        runValidators: true,
+                    }
+                );
+
+                if (!updatedPost) {
+                    throw new Error("Post not found");
+                }
+
+                return newComment; // return comment including username property
+            } catch (error) {
+                throw new Error(`Error adding comment: ${error.message}`);
+            }
+        },
+
+
+        updateComment: async (parent, { commentId, commentText }, context) => {
+            console.log("test updateComment");
+            if (!context.user) {
+                throw new AuthenticationError(
+                    "You need to be logged in to update a comment"
+                );
+            }
+            try {
+                const updatedComment = await Comment.findOneAndUpdate(
+                    {
+                        _id: commentId,
+                        commentAuthor: context.user._id,
+                    },
+                    { $set: { commentText } },
+                    { new: true }
+                );
+
+                if (!updatedComment) {
+                    throw new Error("Comment not found");
+                }
+
+                return updatedComment;
+            } catch (error) {
+                throw new Error(`Error updating comment: ${error.message}`);
+            }
+        },
+        removeComment: async (parent, { commentId }, context) => {
+            console.log("test removeComment");
+            if (!context.user) {
+                throw new AuthenticationError(
+                    "You need to be logged in to remove a comment"
+                );
+            }
+            try {
+                const deletedComment = await Comment.findOneAndDelete({
+                    _id: commentId,
+                    commentAuthor: context.user._id,
+                });
+
+                if (!deletedComment) {
+                    throw new Error("Comment not found");
+                }
+
+                return deletedComment;
+            } catch (error) {
+                throw new Error(`Error removing comment: ${error.message}`);
+            }
+        },
+
+
+        donate: async (parent, { amount }, context) => {
+            console.log(stripe);
+            try {
+                const url = new URL(context.headers.referer).origin;
+                const product = await stripe.products.create({
+                    name: "Donation to PostGame",
+                });
+                const price = await stripe.prices.create({
+                    product: product.id,
+                    unit_amount: amount * 100,
+                    currency: "usd",
+                });
+                const session = await stripe.checkout.sessions.create({
+                    payment_method_types: ["card"],
+                    line_items: [
+                        {
+                            price: price.id,
+                            quantity: 1,
+                        },
+                    ],
+                    mode: "payment",
+                    success_url: `${url}/donation-success`,
+                    cancel_url: `${url}/donation-cancelled`,
+                });
+                return { session: session.id };
+            } catch (error) {
+                console.error("Error in donate function:", error.message);
+                throw error;
+            }
+        },
     },
-    updateComment: async (parent, { commentId, commentText }, context) => {
-      console.log("test updateComment");
-      if (!context.user) {
-        throw new AuthenticationError(
-          "You need to be logged in to update a comment"
-        );
-      }
-      try {
-        const updatedComment = await Comment.findOneAndUpdate(
-          {
-            _id: commentId,
-            commentAuthor: context.user._id,
-          },
-          { $set: { commentText } },
-          { new: true }
-        );
-
-        if (!updatedComment) {
-          throw new Error("Comment not found");
-        }
-
-        return updatedComment;
-      } catch (error) {
-        throw new Error(`Error updating comment: ${error.message}`);
-      }
-    },
-    removeComment: async (parent, { commentId }, context) => {
-      console.log("test removeComment");
-      if (!context.user) {
-        throw new AuthenticationError(
-          "You need to be logged in to remove a comment"
-        );
-      }
-      try {
-        const deletedComment = await Comment.findOneAndDelete({
-          _id: commentId,
-          commentAuthor: context.user._id,
-        });
-
-        if (!deletedComment) {
-          throw new Error("Comment not found");
-        }
-
-        return deletedComment;
-      } catch (error) {
-        throw new Error(`Error removing comment: ${error.message}`);
-      }
-    },
-    donate: async (parent, { amount }, context) => {
-      console.log(stripe);
-      try {
-        const url = new URL(context.headers.referer).origin;
-        const product = await stripe.products.create({
-          name: "Donation to PostGame",
-        });
-        const price = await stripe.prices.create({
-          product: product.id,
-          unit_amount: amount * 100,
-          currency: "usd",
-        });
-        const session = await stripe.checkout.sessions.create({
-          payment_method_types: ["card"],
-          line_items: [
-            {
-              price: price.id,
-              quantity: 1,
-            },
-          ],
-          mode: "payment",
-          success_url: `${url}/donation-success`,
-          cancel_url: `${url}/donation-cancelled`,
-        });
-        return { session: session.id };
-      } catch (error) {
-        console.error("Error in donate function:", error.message);
-        throw error;
-      }
-    },
-  },
 };
 module.exports = resolvers;
