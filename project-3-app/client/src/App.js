d3e3bcfca0eff8befe67a756e59cd968c048ec45
Brandon Mountan <brandonmountan1@gmail.com>
2023-09-28 20:22:55 -0600

    overhauling navbar to try and get login working

--- project-3-app/client/src/App.js
+++ project-3-app/client/src/App.js
@@ -1,40 +1,73 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import {
-    ApolloClient,
-    InMemoryCache,
-    ApolloProvider,
-    createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
-import Container from "./components/Container";
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import Container from "./components/Container";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./components/NavBar";
+import Home from "./components/pages/Home";
+import Contact from "./components/pages/Contact";
+import About from "./components/pages/About";
+import ProfilePost from "./components/pages/PostForm";
+import Login from "./components/pages/Login";
+import Signup from "./components/pages/Signup";
+import Profile from "./components/pages/Profile";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 
 const httpLink = createHttpLink({
-    uri: '/graphql',
+  uri: "/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
-    const token = localStorage.getItem('id_token');
-    return {
-        headers: {
-            ...headers,
-            authorization: token ? `Bearer ${token}` : '',
-        },
-    };
+  const token = localStorage.getItem("id_token");
+  console.log(token);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
 });
 
 const client = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache(),
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
 });
 
-
-const App = () =>
+function App() {
+  return (
     <ApolloProvider client={client}>
-        <Container />;
-    </ApolloProvider>;
-
+      <Router>
+        <div>
+          <Header />
+          <NavBar />
+          <Sidebar />
+          <div>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/me" element={<Profile />} />
+              <Route path="/profiles/:username" element={<Profile />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/post" element={<ProfilePost />} />
+            </Routes>
+          </div>
+          <Footer />
+        </div>
+      </Router>
+    </ApolloProvider>
+  );
+}
 export default App;
