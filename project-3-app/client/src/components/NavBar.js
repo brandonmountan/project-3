85cae73177102e1066a64945d6b3d033582912c9
Brandon Mountan <brandonmountan1@gmail.com>
2023-10-01 17:43:51 -0600

    fixing navbar for my branch

--- project-3-app/client/src/components/NavBar.js
+++ project-3-app/client/src/components/NavBar.js
@@ -1,28 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import Auth from "./utils/auth";
 
-  function NavBar() {
-    if (Auth.loggedIn()) {
-      return (
-        <>
-          <Navbar bg="dark" data-bs-theme="dark">
-            <Container>
-              <Navbar.Brand href="#home">PostGame</Navbar.Brand>
-              <Nav variant="underline" className="me-auto">
-                <Nav.Link href="/home">Home</Nav.Link>
-                <Nav.Link href="/about">About</Nav.Link>
-                <Nav.Link href="/contact">Contact</Nav.Link>
-                <Nav.Link href="/gamepage">GamePage</Nav.Link>
-                <Nav.Link href="/me">{Auth.getProfile().data.username}'s Profile</Nav.Link>
-                <Nav.Link href="/" onClick={() => Auth.logout()}>Logout</Nav.Link>
-=======
-=======
-
->>>>>>> 520dbd37a32507639ee1a42c98ef3dae8601102c
 import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
@@ -64,10 +40,6 @@ function NavBar() {
                 <Nav.Link href="/"
                   className={location.pathname === '/logout' ? 'active' : ''}
                   onClick={() => Auth.logout()}>Logout</Nav.Link>
-<<<<<<< HEAD
->>>>>>> ad406854c3613ebcfb31f9d5353c8123462c0770
-=======
->>>>>>> 520dbd37a32507639ee1a42c98ef3dae8601102c
               </Nav>
             </Navbar.Collapse>
           </Container>
