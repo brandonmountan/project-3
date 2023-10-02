76c66b29b2d599551bf081b5c7fe18fc6aa8dac3
Brandon Mountan <brandonmountan1@gmail.com>
2023-10-01 21:39:42 -0600

    trying to set up live db

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,19 +1,6 @@
-const Sequelize = require('sequelize');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/post-game-db');
 
 module.exports = mongoose.connection;
-
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    });
-
-module.exports = sequelize;
-
-// module.exports.API_KEY = process.env.REACT_API_KEY;
