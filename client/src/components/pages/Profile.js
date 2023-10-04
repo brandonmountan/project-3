45a0662162e29f3034c934d6765aecb7b312c4cb
02madmax <maxlamthach10@gmail.com>
2023-10-04 03:53:20 -0600

    resolved merge

--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -1,33 +1,32 @@
-import  React  from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import Auth from '../utils/auth';
+import { QUERY_ME, QUERY_POSTS } from '../utils/queries';
 import Card from 'react-bootstrap/Card';
-<<<<<<< HEAD
 import { useMutation } from '@apollo/client';
 import { ADD_POST } from '../utils/mutations';
 import { useNavigate } from 'react-router-dom';
-=======
-import { QUERY_ME, QUERY_POSTS } from '../utils/queries';
->>>>>>> origin/develop
 
 function Profile() {
   const navigate = useNavigate();
   const { username: userParam } = useParams();
-<<<<<<< HEAD
-  const { loading, data } = useQuery( QUERY_ME );
-  console.log(data)
-  const user = data?.me || {};
+  const { loading, data: dataMe } = useQuery(QUERY_ME);
+  const { loading: loadingPosts, data } = useQuery(QUERY_POSTS, {variables: {username: userParam}});
+  const user = dataMe?.me || {};
+  const userPosts = data?.posts || [];
+
+
   const [addPost, { error }] = useMutation(ADD_POST);
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
-  
+
     const postTitleValue = event.target.elements.postTitle.value;
     const postTextValue = event.target.elements.postText.value;
     const gameValue = event.target.elements.game.value;
-  
-    const { data } = await addPost({
+
+    const mutationResponse = await addPost({
       variables: {
         postTitle: postTitleValue,
         postText: postTextValue,
@@ -36,35 +35,23 @@ function Profile() {
       },
     });
 
-    console.log(data);
+    console.log(mutationResponse);
 
-  
-    if (!data) {
+    if (!mutationResponse.data) {
       throw new Error('something went wrong!');
     }
     alert('Post created!');
     navigate('/home')
   }
-  
-=======
-  
-  // Use the useQuery hook to fetch the 'me' and 'posts' queries
-  const { loading: loadingMe, data: dataMe } = useQuery(QUERY_ME);
-  const { loading: loadingPosts, data: dataPosts } = useQuery(QUERY_POSTS, {
-    variables: { username: userParam }, // Pass the 'username' variable to the query
-  });
 
-  console.log(dataMe)
-  const user = dataMe?.me || {};
-  const userPosts = dataPosts?.posts || [];
->>>>>>> origin/develop
+
 
   let message = '';
 
   //this assigns profile to "me" properly now
   if (Auth.loggedIn()) {
     if (dataMe) {
-      console.log(dataMe)
+      console.log(data)
 
       // User is viewing their own profile
       message = `Welcome to your profile, ${user.username}!`;
@@ -87,15 +74,14 @@ function Profile() {
   } else {
     // User is not logged in
     message = `You're viewing another user's profile, ${userParam}.`;
-  }
+  } 
 
-  if (loadingMe || loadingPosts) {
+  if (loading || loadingPosts) {
     return <div>Loading...</div>;
   }
 
   return (
     <>
-<<<<<<< HEAD
       <Card className="m-5">
         <Card.Body className="p-5">
           <p>{message}</p>
@@ -112,11 +98,11 @@ function Profile() {
           )}
         </Card.Body>
       </Card>
-  
+
       {Auth.loggedIn() && (
         <div className="m-5">
           <h4>Create a New Post</h4>
-  
+
           {/* This is where your form should be placed */}
           <form onSubmit={handleFormSubmit}>
             <div className="mb-3">
@@ -133,38 +119,10 @@ function Profile() {
             </div>
             <button type="submit" className="btn btn-primary">Add Post</button>
           </form>
-  
+
           {error && <div className="mt-3 text-danger">Error creating post!</div>}
         </div>
       )}
-=======
-    <Card className="m-5">
-      <Card.Body className="p-5">
-        <p>{message}</p>
-        {Auth.loggedIn() ? (
-          // me
-          <div>
-          <h2>Your Posts</h2>
-          {userPosts.map((post) => (
-            <li key={post._id}>
-              <h3>{post.postTitle}</h3>
-              <p>{post.postText}</p>
-              <p>Posted by: {post.postAuthor}</p>
-              <p>Created at: {post.createdAt}</p>
-            </li>
-          ))}
-          </div>
-        ) : (
-          // other user
-          <p>Additional content for others viewing the profile.</p>
-        )}
-        {/* Placeholder content */}
-        {userParam !== 'me' && !Auth.loggedIn() && (
-          <p>This is a placeholder for the public profile page.</p>
-        )}
-      </Card.Body>
-    </Card>
->>>>>>> origin/develop
     </>
   );
 }
